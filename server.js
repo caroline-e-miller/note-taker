7bac3aa6c06a552b6726e3fb5d627f31e3447513
caroline-e-miller <caroline.miller@ymail.com>
2021-03-17 15:29:46 -0400

    created routes, server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
