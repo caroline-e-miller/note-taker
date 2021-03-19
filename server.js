c3bcb82e2faf4526d6061791300f2bf3b1f985f5
caroline-e-miller <caroline.miller@ymail.com>
2021-03-19 19:41:25 -0400

    added comments

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// setting up dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+// declares express as the server
 const app = express();
+// sets up port
 const PORT = 3000;
 
+// sets up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+// gives server routes to respond through
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
+// sets up listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
